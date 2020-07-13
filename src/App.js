e85a30355d80e5827cff3579226a81c4913cd666
isaacindra <iacarrillo@indracompany.com>
2020-07-12 22:21:59 -0500

    Segundo Commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p> 
-        Estamos en Development
+        Estamos en Development - 2 cambio
         </p>
         <a
           className="App-link"
