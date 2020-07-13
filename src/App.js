5fdc728bd8271cc8aa2113488385311a2c220667
isaacindra <iacarrillo@indracompany.com>
2020-07-12 22:07:13 -0500

    Development

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p> 
-        Estamos en Delivery
+        Estamos en Development
         </p>
         <a
           className="App-link"
